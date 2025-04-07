885e57347c4452a40628bafc40d491b6f293dded
Farrel Tito <tito_farrel@yahoo.co.id>
2025-04-08 03:38:00 +0700

    Variable without Data Type

--- golang-session-1/main.go
+++ golang-session-1/main.go
@@ -15,4 +15,5 @@ func main() {
 	fmt.Print("Hello World! this is me Farrel Tito\n")         // \n for enter, kalau tidak pakai \n akan nempel (C)
 	fmt.Print("Hello World!", " this is me Farrel Tito\n")     // same like statement B, but we should add "Space", bcs no space by default (D)
 	fmt.Print("Hello World!", " ", "this is me Farrel Tito\n") // same like statement D, but we should add "Space" in the middle of text
+}
 */
--- golang-session-1/variables.go
+++ golang-session-1/variables.go
@@ -4,20 +4,21 @@ import "fmt" //fmt is a "Format Package"
 
 /*
 	This is a Hello World Project.
-	Testing a variation of Println and Print.
+	Testing a variation of Println and Printf.
 */
 
-// Variable with data type
+// Variable without data type
 func main() {
-	var name string = "Tito" //set data or re-assign
+	var name string = "Tito" //set data
 	var age int = 25
 
-	name = "Mr 4k" //set data #2 (Change)
+	name = "Mr 4k" //set data #2 or re-assign (Change)
 	age = 20
 
-	fmt.Println("This author name is", name)
-	fmt.Println("and his/her age is", age)
+	fmt.Printf("%T, %T", name, age) //%T sesuai dengan posisi, yg pertama buat name, yang kedua buat age
 }
+
+// Result: "This author name is%!(EXTRA string=Mr 4k)and his/her age is 20"
 
 /*
 	#### Note ####
