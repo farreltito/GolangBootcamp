d738d800d6893c4b3c9df6b3a839bb8e0df623a1
Farrel Tito <tito_farrel@yahoo.co.id>
2025-04-08 02:47:19 +0700

    Variable with Data Type & Tidying Dependencies

--- golang-session-1/main.go
+++ golang-session-1/main.go
@@ -1,11 +1,12 @@
 package main
 
-import "fmt" //fmt is a "Format Package"
-
 /*
+import "fmt" //fmt is a Format Package:
+
+
 	This is a Hello World Project.
 	Testing a variation of Println and Print.
-*/
+
 
 func main() {
 	fmt.Println("Hello World! this is me Farrel Tito")    //make a space without comma (in 1 string variable) (A)
@@ -14,4 +15,4 @@ func main() {
 	fmt.Print("Hello World! this is me Farrel Tito\n")         // \n for enter, kalau tidak pakai \n akan nempel (C)
 	fmt.Print("Hello World!", " this is me Farrel Tito\n")     // same like statement B, but we should add "Space", bcs no space by default (D)
 	fmt.Print("Hello World!", " ", "this is me Farrel Tito\n") // same like statement D, but we should add "Space" in the middle of text
-}
+*/
---
+++ golang-session-1/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt" //fmt is a "Format Package"
+
+/*
+	This is a Hello World Project.
+	Testing a variation of Println and Print.
+*/
+
+// Variable with data type
+func main() {
+	var name string = "Tito" //set data or re-assign
+	var age int = 25
+
+	name = "Mr 4k" //set data #2 (Change)
+	age = 20
+
+	fmt.Println("This author name is", name)
+	fmt.Println("and his/her age is", age)
+}
+
+/*
+	#### Note ####
+	Advantages of Go languages:
+	- All variables used.
+*/
