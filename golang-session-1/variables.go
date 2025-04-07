cd3e38a24763d8f9565d11e54fd0251ca4df1fa8
Farrel Tito <tito_farrel@yahoo.co.id>
2025-04-08 04:48:10 +0700

    fmt.Printf Usage (verb)

--- golang-session-1/variables.go
+++ golang-session-1/variables.go
@@ -1,15 +1,27 @@
 package main
 
+import "fmt"
+
 /*
 	This is a Variable Project.
 	Testing a variation of Println and Printf.
 */
 
-// Underscore Variable
+// fmt.Printf Usage (verb)
 func main() {
-	var aVariable string
-	var companyname, companyage, companyaddress = "Sage", 2, "Jl. Jalan Happy"
+	var companyname, companyaddress string = "Sage", "Jl. Jalan Happy"
+	number1, number2, number3, companyage := 1.22, "5", 3, 7
 
-	// Underscore digunakan untuk menghindari error ketika variable tidak digunakan
-	_, _, _, _ = aVariable, companyname, companyage, companyaddress
+	fmt.Printf("Data type of this variable is %T \n", number1)
+	fmt.Printf("Data type of this variable is %T \n", number2)
+	fmt.Printf("Data type of this variable is %T \n", number3)
+	fmt.Printf("This company name is %s, this company age is %d, and this company address is at %s", companyname, companyage, companyaddress)
 }
+
+/*
+	Result:
+	Data type of this variable is float64
+	Data type of this variable is string
+	Data type of this variable is int
+	This company name is Sage, this company age is 7, and this company address is at Jl. Jalan Happy
+*/
