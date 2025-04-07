1e64784470f0e9db33670d0883b7a14fc3feee79
Farrel Tito <tito_farrel@yahoo.co.id>
2025-04-07 18:22:34 +0700

    Variation of Hello World (Commit 3)

--- golang-session-1/main.go
+++ golang-session-1/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import "fmt" //fmt is a "Format Package"
+
+/*
+	This is a Hello World Project.
+	Testing a variation of Println and Print.
+*/
 
 func main() {
-	fmt.Println("Hello World! this is me Farrel Tito")
+	fmt.Println("Hello World! this is me Farrel Tito")    //make a space without comma (in 1 string variable) (A)
+	fmt.Println("Hello World!", "this is me Farrel Tito") //make a space with comma or separator (B)
+
+	fmt.Print("Hello World! this is me Farrel Tito\n")         // \n for enter, kalau tidak pakai \n akan nempel (C)
+	fmt.Print("Hello World!", " this is me Farrel Tito\n")     // same like statement B, but we should add "Space", bcs no space by default (D)
+	fmt.Print("Hello World!", " ", "this is me Farrel Tito\n") // same like statement D, but we should add "Space" in the middle of text
 }
